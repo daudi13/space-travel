fa2b6b4c772b010e2263ecbb359a4f0a3307596f
daudi13 <davouma13@gmail.com>
2022-04-11 17:46:12 +0300

    refactor jsx to receive props

---
+++ src/components/RocketsCard.js
@@ -0,0 +1,17 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+const RocketCard = () => (
+  <div className="rocket">
+    <image scr="https://i.imgur.com/DaCfMsj.jpg" alt="rocker" />
+    <div className="rocket--desc">
+      <h2 className="rocket--desc__title">Falcon 1</h2>
+      <p className="rocket--desc__info">
+        j
+      </p>
+      <button type="button">Reserve Rocket</button>
+    </div>
+  </div>
+);
+
+export default RocketCard;

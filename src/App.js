f48113bea859b2308aca166acfc0555d93215e0a
daudi13 <davouma13@gmail.com>
2022-04-12 06:45:15 +0300

    link Rockket to App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Missions from './components/Missions';
 import NavBar from './components/NavBar';
+import Rocket from './components/Rocket/Rocket';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
+        <Route path="/" element={<Rocket />} />
         <Route path="/missions" element={<Missions />} />
       </Routes>
     </BrowserRouter>

6f3cf9cac10b7417c0110628cad4d640af431ee9
daudi13 <davouma13@gmail.com>
2022-04-13 10:49:14 +0300

    Add provider in App()

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Missions from './components/Missions/Missions';
 import NavBar from './components/Missions/NavBar';
 import Rocket from './components/Rocket/Rocket';
+import store from './redux/configureStore';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Rocket />} />
-        <Route path="/missions" element={<Missions />} />
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Rocket />} />
+          <Route path="/missions" element={<Missions />} />
+        </Routes>
+      </Provider>
     </BrowserRouter>
   );
 }

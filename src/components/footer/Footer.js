d9760eabaca98ea90541098938ba546b9a208abf
daudi13 <davouma13@gmail.com>
2022-04-14 16:09:18 +0300

    Add footer color

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => (
-  <footer>
+  <footer className="footer">
     <p>
       Copyright&copy;
       {new Date().getFullYear()}

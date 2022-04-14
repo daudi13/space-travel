ff2e2db19e208c3a9634fab5c40f51fa5eeb59e0
daudi13 <davouma13@gmail.com>
2022-04-14 17:52:51 +0300

    Add dragonreducer in configureStore.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,10 +3,12 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rocketReducer from './Rockets/Rockets';
 import missionsReducer from './Missions/missions';
+import dragonReducer from './dragons/dragons';
 
 const root = combineReducers({
   rockets: rocketReducer,
   missions: missionsReducer,
+  dragons: dragonReducer,
 });
 
 const store = createStore(root, applyMiddleware(logger, thunk));

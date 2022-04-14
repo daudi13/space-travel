14bcba565cd1f9427cde74cdf4d5a8b9fead4abe
daudi13 <davouma13@gmail.com>
2022-04-14 15:53:57 +0300

    Add footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <p>
+      Copyright&copy;
+      {new Date().getFullYear()}
+      {' '}
+    </p>
+  </footer>
+);
+
+export default Footer;

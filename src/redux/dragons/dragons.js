4f63609952f58c9fc233556edd637234c8d0347b
daudi13 <davouma13@gmail.com>
2022-04-14 17:20:20 +0300

    Add action function

--- src/redux/dragons/dragons.js
+++ src/redux/dragons/dragons.js
@@ -1,4 +1,4 @@
-import FetchDragons from "../APIs/dragonsAPI";
+import FetchDragons from '../APIs/dragonsAPI';
 
 const GET_DRAGONS_REQUEST = 'space-travel/dragons/GET_REQUEST';
 const GET_DRAGONS_SUCCESS = 'space-travel/dragons/GET_SUCCESS';
@@ -11,3 +11,38 @@ const initialState = {
   error: '',
 };
 
+const getDragonsRequest = () => ({
+  type: GET_DRAGONS_REQUEST,
+});
+
+const getDragonsSuccess = (dragonData) => ({
+  type: GET_DRAGONS_SUCCESS,
+  payload: dragonData,
+});
+
+const getDragonFail = (error) => ({
+  type: GET_DRAGONS_FAIL,
+  payload: error,
+});
+
+const getDragons = () => (dispatch) => {
+  dispatch(getDragonsRequest());
+  FetchDragons()
+    .then((data) => {
+      const dragonData = data.map((uniData) => ({
+        id: uniData.id,
+        name: uniData.name,
+        desc: uniData.description,
+        image: uniData.flickr_image[0],
+      }));
+      dispatch(getDragonsSuccess(dragonData));
+    })
+    .catch((error) => {
+      dispatch(getDragonFail(error.message));
+    });
+};
+
+const reserveDragons = (id) => ({
+  type: RESERVE_DRAGONS,
+  payload: id,
+});

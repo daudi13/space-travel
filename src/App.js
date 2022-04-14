c9070b1ce39c2f26060a2b3abc20160ffef98091
daudi13 <davouma13@gmail.com>
2022-04-14 18:33:23 +0300

    Add dragons to App Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dragon from './components/dragons/Dragon';
 import Footer from './components/footer/Footer';
 import Missions from './components/Missions/Missions';
 import NavBar from './components/Missions/NavBar';
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<Rocket />} />
           <Route path="/missions" element={<Missions />} />
           <Route path="/my-profile" element={<MyProfile />} />
+          <Route path="/dragons" element={<Dragon />} />
         </Routes>
       </Provider>
       <Footer />
